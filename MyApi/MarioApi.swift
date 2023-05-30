97416c2800bc8076b1785b2f9e8249dc4c370747
Mohammed <mohammed@Mohammeds-MacBook-Pro.local>
2023-05-30 15:02:59 +0300

    second commit

---
+++ MyApi/MarioApi.swift
@@ -0,0 +1,20 @@
+//
+//  MarioApi.swift
+//  MyApi
+//
+//  Created by Mohammed on 10/11/1444 AH.
+//
+
+import SwiftUI
+
+struct MarioApi: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MarioApi_Previews: PreviewProvider {
+    static var previews: some View {
+        MarioApi()
+    }
+}
